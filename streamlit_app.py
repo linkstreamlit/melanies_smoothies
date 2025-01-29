7a178cfbe05bf321a6b3d2c93a07d25c7b14dfbe
linkstreamlit <lingeshstocks@gmail.com>
2025-01-29 16:27:02 +0530

    Update streamlit_app.py
    moved the import function into line no:4 and moved the last newly added code into "FOR" loop area

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write("""Choose the fruits you want in your custom smoothie!""")
@@ -24,6 +25,8 @@ ingredients_string = ""
 if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width = True)
 
 my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """', ' """ +name_on_order+ """ ')"""
@@ -35,7 +38,4 @@ if time_to_insert:
     st.success('Your Smoothie is Ordered!', icon="✅")
 
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width = True)
+

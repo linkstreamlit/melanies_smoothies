311c65b4a2286b76b46e0422c0f1d89c8fcb0b81
linkstreamlit <lingeshstocks@gmail.com>
2025-01-31 09:32:27 +0530

    Update streamlit_app.py
    error changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ if ingredients_list:
 
         st.subheader(fruit_chosen+' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
-        #sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width = True)
+        fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width = True)
 
 my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """', ' """ +name_on_order+ """ ')"""
